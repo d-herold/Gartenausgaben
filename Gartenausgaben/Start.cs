2c9e7e2d31795abda4945ae513d98208adb04a5d
Daniel Herold <d.herold@bfwth-online.de>
2020-10-30 10:13:38 +0100

    Funktion zum Schließen des Hauptfensters

--- Gartenausgaben/Start.cs
+++ Gartenausgaben/Start.cs
@@ -35,3 +35,76 @@ namespace Gartenausgaben
 
     }
 }
+
+/*
+public class MyWindow : Form
+{
+   //-------------------------------------------------------------
+   static int iId    = 1;
+   #if !SW_MAINWINDOW
+      static int iCount = 0;
+   #endif
+
+   //=============================================================
+   public MyWindow ()
+   {
+      Control ctrlCurr;
+
+
+      //----------------------------------------------------------
+      #if !SW_MAINWINDOW
+         ++iCount;
+      #endif
+      Text = "Window " + iId++;
+      Size = new Size (220 + Size.Width - ClientSize.Width,
+                        70 + Size.Height - ClientSize.Height);
+
+      //----------------------------------------------------------
+      ctrlCurr = new Button ();
+      ctrlCurr.Location = new Point (10,10);
+      ctrlCurr.Size = new Size (200, 50);
+      ctrlCurr.Text = "New Window";
+      ctrlCurr.Click += new EventHandler (ButtonClick);
+      Controls.Add (ctrlCurr);
+
+      //----------------------------------------------------------
+      #if !SW_MAINWINDOW
+         Disposed += new EventHandler (MyWindowDisposed);
+         Show ();
+      #endif
+   }
+
+   //=============================================================
+   protected void ButtonClick (Object sender, EventArgs e)
+   {
+      MyWindow myw = new MyWindow ();
+   }
+
+   //=============================================================
+   #if !SW_MAINWINDOW
+      protected void MyWindowDisposed (Object sender, EventArgs e)
+      {
+         if (--iCount <= 0) {
+            Application.Exit ();
+         }
+      }
+   #endif
+}
+
+//****************************************************************
+abstract class App
+{
+   //=============================================================
+   public static int Main (string [] astrArg)
+   {
+      #if !SW_MAINWINDOW
+         new MyWindow ();
+         Application.Run ();
+      #else
+         Application.Run (new MyWindow ());
+      #endif
+
+      return 0;
+   }
+}
+ */
