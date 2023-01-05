8cf37c039bb5a811ac2ac59a865e2932a04a1539
Daniel Herold <d.herold@bfwth-online.de>
2023-01-05 10:00:41 +0100

    Markt gelöscht

---
+++ Gartenausgaben/Markt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gartenausgaben
+{
+    abstract class Markt
+    {
+        public string Beton { get; }
+    }
+}
